f427fc2174ce771c0f70936a8c5c2d97848283ab
bushidog <bushidog@student.21-school.ru>
2024-05-01 18:57:20 +0300

    'upd'

---
+++ src/ex02/src/main/java/edu/school21/repositories/ProductsRepositoryJdbcImpl.java
@@ -0,0 +1,96 @@
+package edu.school21.repositories;
+
+import edu.school21.models.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+
+public class ProductsRepositoryJdbcImpl implements ProductsRepository{
+
+    private final DataSource dataSource;
+
+    public ProductsRepositoryJdbcImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Override
+    public List<Product> findAll() {
+        ArrayList<Product> productArrayList = new ArrayList<>();
+
+        String allQuery = "SELECT * FROM tests.product;";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(allQuery)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Product product = new Product(resultSet.getInt(1),
+                        resultSet.getString(2), resultSet.getInt(3));
+                productArrayList.add(product);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return productArrayList;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        Optional<Product> optionalProduct;
+        String selectQuery = "SELECT * FROM tests.product WHERE id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(selectQuery)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            optionalProduct = Optional.of(new Product(resultSet.getInt(1),
+                    resultSet.getString("name"), resultSet.getInt("cost")));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return optionalProduct;
+    }
+
+    @Override
+    public void update(Product product) {
+        String updateQuery = "UPDATE tests.product SET name = ?, cost = ? WHERE id = ?;";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(updateQuery)) {
+            statement.setString(1, product.getName());
+            statement.setInt(2, product.getCost());
+            statement.setLong(3, product.getId());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void save(Product product) {
+        String insertQuery = "INSERT INTO tests.product (id, name, cost) VALUES ( ?, ?, ?);";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+            statement.setLong(1, product.getId());
+            statement.setString(2, product.getName());
+            statement.setInt(3, product.getCost());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete ( Long id ) {
+        String deleteQuery = "DELETE FROM tests.product WHERE id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(deleteQuery)) {
+            statement.setLong(1, id);
+            statement.execute();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
